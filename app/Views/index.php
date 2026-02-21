0245d1b8cd333faecf3a05faef4ed4a265a2f22f
eekka <rizqiekapramudyatama@google.com>
2026-02-21 22:32:12 +0700

    maps,section,

--- app/Views/index.php
+++ app/Views/index.php
@@ -15,8 +15,6 @@
     </head>
     <body class="bg-blue-100">
          <style>
-            /* font */
-            /* @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap"); */
             /* menu navbar-humbergermenu */
             .nav-link {
             position: relative;
@@ -62,6 +60,15 @@
             .swiper-item {
             font-size: 0.5rem;
             }
+            /* animasi float */
+            @keyframes float {
+                0%, 100% { transform: translateY(0) translateX(-50%); }
+                50% { transform: translateY(-15px) translateX(-50%); }
+            }
+            .float-animation {
+                animation: float 2s ease-in-out infinite;
+            }
+
         </style>
 
         <!-- whatsapp -->
@@ -133,7 +140,7 @@
         <!-- /navbar -->
         
         <!-- Section Hero -->
-         <section id="home" class="pt-36 flex items-center justify-center h-screen w-full overflow-hidden">
+         <section id="home" class=" flex items-center justify-center h-screen w-full overflow-hidden">
                 <div class="h-screen w-full overflow-hidden relative swiper mySwiper">
                     <div class="swiper-wrapper">
                     <!-- slide -->
@@ -200,10 +207,6 @@
         </div>
         </section>
         <!-- /Section Hero -->
-        <!-- Hero Slider Section -->
-        <section id="home" class="w-full h-screen overflow-hidden">
-
-        </section>
 
         <!-- About Section -->
         <section id="tentang"class="pt-36" >
@@ -242,8 +245,9 @@
         <!-- /about section -->
 
         <!-- Gedung section -->
-        <section class="py-20" id="fasilitas">
+        <section class="py-10" id="fasilitas">
             <div class=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <p class="text-3xl font-bold text-center heading-modern bounce-3s">Gedung & Ruangan</p>
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
                     <!-- Building Cards -->
                     <div class="bg-white rounded shadow-xl overflow-hidden card-hover hover:shadow-2xl transition-shadow text-left h-full flex flex-col">
@@ -448,39 +452,6 @@
         </section>
         <!-- /fasilitas section -->
 
-        <!-- kontak Section -->
-        <section id="kontak" class="py-20 bg-blue-600 text-white relative overflow-hidden">
-            <div class="w-1/2">
-                <div class="absolute inset-0 opacity-10">
-                    <div class="absolute top-0 left-0 w-96 h-96 bg-white rounded-full -translate-x-1/2 -translate-y-1/2"></div>
-                    <div class="absolute bottom-0 right-0 w-96 h-96 bg-white rounded-full translate-x-1/2 translate-y-1/2"></div>
-                </div>
-                <div class="relative flex w-full mx-auto sm:px-6 lg:px-8 text-center">
-                    <div class="w-1/2 flex-1">
-                        <h2 class="text-4xl md:text-5xl font-bold mb-6">Tertarik untuk Menggunakan Layanan Kami?</h2>
-                        <p class="text-xl md:text-2xl mb-10 text-gray-100">Kunjungi atau Hubungi tim kami untuk informasi lebih lanjut</p>
-                        <a href="https://wa.me/6289670131732" target="_blank" class="inline-flex items-center gap-3 bg-white text-purple-600 px-10 py-4 rounded-full font-bold hover:bg-gray-100 hover:scale-105 transition-all shadow-2xl">
-                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z"/>
-                            </svg>
-                            Hubungi Kami Sekarang
-                        </a>
-                    </div>                             
-                </div>
-                <!-- Lokasi Kami Section - Full Width -->
-                <div class="w-1/2 flex-2">
-                    <div class="justify-center">
-                        <h3 class="text-xl font-bold mb-4 text-white"></h3>
-                    </div>
-                    <div class="relative w-full h-64 overflow-hidden shadow-lg">
-                        <iframe src="https://maps.google.com/maps?q=Balatkop+UMKM,+Semarang&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=&amp;output=embed" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-                        </iframe>
-                    </div>
-                </div> 
-            </div>
-        </section>
-        <!-- /kontak -->
-
 
             <!-- Gallery Section -->
             <section id="galeri" class="py-20">
@@ -546,6 +517,25 @@
                 </div>
             </section>
         <!-- /gallery section -->
+
+        
+        <!-- kontak Section -->
+        <section id="kontak" class="py-20 bg-blue-600 text-white relative overflow-hidden">
+            <div class="">
+                <div class="absolute inset-0 opacity-10">
+                    <div class="absolute top-0 left-0 w-96 h-96 bg-white rounded-full -translate-x-1/2 -translate-y-1/2"></div>
+                    <div class="absolute bottom-0 right-0 w-96 h-96 bg-white rounded-full translate-x-1/2 translate-y-1/2"></div>
+                </div>
+                <div class="relative flex w-full mx-auto sm:px-6 lg:px-8 text-center">
+                    <div class="flex-1">
+                        <h2 class="text-4xl md:text-5xl font-bold mb-6">Tertarik untuk Menggunakan Layanan Kami?</h2>
+                        <p class="text-xl md:text-2xl mb-10 text-gray-100"><span class="font-bold text-green-500">Kunjungi atau Hubungi</span> tim kami untuk informasi lebih lanjut</p>
+                    </div>                             
+                </div>
+            </div>
+        </section>
+        <!-- /kontak -->
+
 
         <!-- footer -->
         <footer class="bg-gray-900 text-white py-16">
