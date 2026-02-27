0255a17d4e17cb1dda37fd0e1a77faf34020b0c7
eekka <rizqiekapramudyatama@google.com>
2026-02-27 10:30:55 +0700

    paragraf slide hero

--- app/Views/index.php
+++ app/Views/index.php
@@ -634,10 +634,10 @@
         </style>
 
         <!-- whatsapp -->
-        <a class="fixed bottom-6 right-6 z-50 group" href="https://wa.me/6281234567890?text=Halo%20Admin%20Balatkop,%20saya%20ingin%20tanya%20mengenai%20sarana%20prasarana..."target="_blank">                
+        <a class="fixed bottom-4 right-4 z-50 md:z-0 group" href="https://wa.me/6281234567890?text=Halo%20Admin%20Balatkop,%20saya%20ingin%20tanya%20mengenai%20sarana%20prasarana..."target="_blank">                
             <span class="absolute inline-flex h-2 w-2 rounded-full opacity-75"></span>                
-            <div class="relative bg-green-500 text-white p-4 rounded-full shadow-2xl flex items-center justify-center group-hover:scale-110">
-                <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 24 24">
+            <div class="relative bg-green-500 text-white p-3 rounded-full shadow-2xl flex items-center justify-center group-hover:scale-110">
+                <svg class="w-8 h-8 md:w-10 md:h-10" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L0 24l6.335-1.662c1.72.94 3.659 1.437 5.63 1.438h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z"/>
                 </svg>                   
             </div>
@@ -717,7 +717,7 @@
                             <div class="md:max-w-2/3 lg:max-w-1/2 lg:ml-15">
                                 <p class="text-lg reveal-left reveal-on-scroll">Slide 1</p>
                                 <h1 class="text-9xl leading-8 mt-4 md:text-6xl font font-bold mb-4 uppercase reveal-left reveal-on-scroll">Selamat Datang di <span class="text-blue-800 text-shadow-2xs text-shadow-white">Sarana Prasarana</span>  Balatkop Jateng</h1>
-                                <p class="text-sm mt-2 md:text-lg mb-8 reveal-left reveal-on-scroll">Mendukung Operasional Instansi Pemerintah dengan Fasilitas Terbaik</p>
+                                <p class="text-sm mt-2 md:text-lg mb-8 reveal-left reveal-on-scroll">Siap melayani kebutuhan koperasi, UKM, dan masyarakat Provinsi Jawa Tenga!</p>
                                 <a href="#tentang" class="reveal-left reveal-on-scroll hero-cta-btn mt-5 text-sm w-62.5 justify-center uppercase inline-flex items-center gap-3 text-white text-decoration-none px-8 py-4 shadow-2xl focus-visible:animate-pulse active:scale-95">
                                     Jelajahi Sekarang
                                 </a>
@@ -731,7 +731,7 @@
                             <div class="md:max-w-2/3 lg:max-w-1/2 lg:ml-15">
                                 <p class="text-lg">Slide 2</p>
                                 <h1 class="text-9xl leading-8 mt-4 md:text-6xl font font-bold mb-4 uppercase">Balatkop UKM Jateng</h1>
-                                <p class="text-sm mt-2 md:text-lg mb-8">Sebuah instansi pelatihan yang memberikan pelatihan dan pendampingan terbaik untuk UKM di Jawa Tengah</p>
+                                <p class="text-sm mt-2 md:text-lg mb-8">Sebuah instansi pelatihan yang memberikan pelatihan dan pendampingan terbaik untuk koperasi dan UKM di Provinsi Jawa Tengah.</p>
                                 <a href="#tentang" class="hero-cta-btn mt-5 text-sm w-62.5 justify-center uppercase inline-flex items-center gap-3 text-white text-decoration-none px-8 py-4 shadow-2xl focus-visible:animate-pulse active:scale-95">
                                     Cari Tahu Lebih Lanjut!
                                 </a>
@@ -745,7 +745,7 @@
                             <div class="md:max-w-2/3 lg:max-w-1/2 lg:ml-15">
                                 <p class="text-lg">Slide 3</p>
                                 <h1 class="text-9xl leading-8 mt-4 md:text-6xl font font-bold mb-4 uppercase">Ruang Kelas & Rapat</h1>
-                                <p class="text-sm mt-2 md:text-lg mb-8">Ruang rapat dengan berbagai fasilitas penunjang kegiatan</p>
+                                <p class="text-sm mt-2 md:text-lg mb-8">Ruangan dengan berbagai fasilitas penunjang acara baik untuk kegiatan pelatihan maupun pertemuan.</p>
                                 <a href="#fasilitas" class="hero-cta-btn mt-5 text-sm w-62.5 justify-center uppercase inline-flex items-center gap-3 text-white text-decoration-none px-8 py-4 shadow-2xl focus-visible:animate-pulse active:scale-95">
                                     Lihat Detail
                                 </a>
@@ -759,7 +759,7 @@
                             <div class="md:max-w-2/3 lg:max-w-1/2 lg:ml-15">
                                 <p class="text-lg">Slide 4</p>
                                 <h1 class="text-9xl leading-8 mt-4 md:text-6xl font font-bold mb-4 uppercase">Gedung Aula</h1>
-                                <p class="text-sm mt-2 md:text-lg mb-8">Ruangan dengan daya tampung besar serta fasilitas fasilitas yang dijamin memuaskan</p>
+                                <p class="text-sm mt-2 md:text-lg mb-8">Ruangan dengan daya tampung yang besar serta fasilitas-fasilitas yang dijamin memuaskan.</p>
                                 <a href="#fasilitas" class="hero-cta-btn mt-5 text-sm w-62.5 justify-center uppercase inline-flex items-center gap-3 text-white text-decoration-none px-8 py-4 shadow-2xl focus-visible:animate-pulse active:scale-95">
                                     Lihat Detail
                                 </a>
@@ -773,7 +773,7 @@
                             <div class="md:max-w-2/3 lg:max-w-1/2 lg:ml-15">
                                 <p class="text-lg">Slide 5</p>
                                 <h1 class="text-9xl leading-8 mt-4 md:text-6xl font font-bold mb-4 uppercase">Laboratorium</h1>
-                                <p class="text-sm mt-2 md:text-lg mb-8">Ruangan dengan berbagai fasilitas yang sesuai dengan kegiatan yang dilaksanakan</p>
+                                <p class="text-sm mt-2 md:text-lg mb-8">Ruangan pelatihan dengan berbagai fasilitas yang sesuai dengan acara atau kegiatan yang dilaksanakan.</p>
                                 <a href="#fasilitas" class="hero-cta-btn mt-5 text-sm w-62.5 justify-center uppercase inline-flex items-center gap-3 text-white text-decoration-none px-8 py-4 shadow-2xl focus-visible:animate-pulse active:scale-95">
                                     Lihat Detail
                                 </a>
@@ -807,8 +807,8 @@
                     <div class="space-y-6 reveal-left reveal-on-scroll">
                         <h3 class="text-3xl font-bold text-white">Deskripsi <span style="color:#FFD700;">Umum</span></h3>
                         <p class="text-white text-lg leading-relaxed">
-                            Sarana dan prasarana kami dirancang untuk mendukung penuh kelancaran operasional instansi pemerintah. 
-                            Dengan infrastruktur modern dan fasilitas lengkap, kami berkomitmen memberikan layanan terbaik kepada masyarakat.
+                            Sarana dan prasarana kami dirancang untuk mendukung penuh kelancaran operasional kegiatan baik formal maupun non-formal. 
+                            Dengan infrastruktur modern dan fasilitas lengkap serta kebersihan yang terjaga, kami berkomitmen memberikan layanan terbaik kepada masyarakat.
                         </p>
                         <p class="text-white text-lg leading-relaxed">
                             Setiap fasilitas telah direncanakan dengan matang untuk memastikan kenyamanan pengguna dan efisiensi kerja maksimal.
@@ -1112,7 +1112,6 @@
         </section>
         <!-- /fasilitas section -->
 
-
         <!-- Gallery Section -->
         <section id="galeri" class="pt-20">
             <div class="w-full bg-blue-950 p-10 mx-auto px-4 sm:px-6 lg:px-8 reveal-on-scroll">
@@ -1262,9 +1261,15 @@
                         <h3 class="text-xl font-bold mb-6 text-white">Jam <span style="color:#FFD700;">Operasional</span></h3>
                         <div class="space-y-4">
                             <div>
-                                <p class="text-gray-400 text-sm"><span class="font-semibold">Senin - Jumat:</span></p>
+                                <p class="text-gray-400 text-sm"><span class="font-semibold">Senin - Kamis:</span></p>
                                 <p class="text-white text-sm">07:00 - 16:00 WIB</p>
+                                <!-- <p class="text-white text-sm">Istirahat: 12:00 - 13:00 WIB</p> -->
                             </div>
+                            <div>
+                                <p class="text-gray-400 text-sm"><span class="font-semibold">Jumat:</span></p>
+                                <p class="text-white text-sm">07:00 - 14:00 WIB</p>
+                                <!-- <p class="text-white text-sm">Istirahat: 12:00 - 13:00 WIB</p> -->
+                            </div>                            
                             <div>
                                 <p class="text-gray-400 text-sm"><span class="font-semibold">Sabtu & Minggu:</span></p>
                                 <p class="text-white text-sm">Tutup</p>
@@ -1302,7 +1307,7 @@
                 <!-- Copyright Section -->
                 <hr class="border-gray-800 mb-8">
                 <div class="text-center text-gray-400 text-sm">
-                    <p>© 2026 Sarana dan Prasarana Balatkop UKM Jateng.</p>
+                    <p>© 2026 Sarana dan Prasarana Balatkop UKM Jateng. Created by KaHa</p>
                 </div>
             </div>
         </footer>
@@ -1433,8 +1438,8 @@
                         <!-- aula anggrek -->
                         <div class="p-6 modal-info-utama modal-info-block" data-slide="0">
                             <div class="flex flex-col items-start">
-                                <div class="bg-blue-600 text-white px-4 py-2 rounded-lg mb-3">
-                                    <h3 class="text-2xl font-bold text-center">Aula Anggrek</h3>
+                                <div class="justify-between bg-blue-600 text-white px-4 py-2 rounded-lg mb-3">            
+                                    <h3 class="text-2xl font-bold text-center">Aula Anggrek</h3>                                    
                                 </div>
                                 <div class="inline-block bg-blue-100 border border-blue-300 px-3 py-2 rounded-lg mb-3">
                                     <p class="text-gray-700 text-sm mb-1">
@@ -2491,7 +2496,7 @@
         <!-- Bootstrap -->  
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script> 
         <script>
-            // Navigation active state
+        // Navigation active state
             /* Menu Toggle */
             function toggleMenu() {
                 const menu = document.getElementById('mobileMenu');
@@ -2549,7 +2554,8 @@
                     }, 260);
                 });
             });
-            // Swiper (Hero)
+        // Swiper
+            // Swiper (hero)
             const swiper = new window.Swiper('.mySwiper', {
                 effect: 'fade',
                 speed: 1000,
@@ -2559,10 +2565,10 @@
                 allowTouchMove: true,
             });
 
-            // Swiper (Hero)
+            // Swiper (img modal)
             const swiperimg = new window.Swiper('.mySwiperimg', {
-                slidesPerView: 3,
-                spaceBetween: 30,
+                slidesPerView: 3.2,
+                spaceBetween: 10,
                 pagination: {
                     el: ".swiper-pagination",
                     clickable: true,
@@ -2662,7 +2668,7 @@
                     slidesPerView: 1.2,
                     centeredSlides: true,
                     spaceBetween: 24,
-                    autoplay: { delay: 3000, disableOnInteraction: true },
+                    autoplay: { delay: 5000, disableOnInteraction: true },
                     pagination: { el: '#swiperAula .swiper-pagination', type: 'fraction' },
                     navigation: {
                         nextEl: '#swiperAula .swiper-button-next',
@@ -2680,7 +2686,7 @@
                     slidesPerView: 1.2,
                     centeredSlides: true,
                     spaceBetween: 24,
-                    autoplay: { delay: 3000, disableOnInteraction: true },
+                    autoplay: { delay: 5000, disableOnInteraction: true },
                     pagination: { el: '#swiperRuang .swiper-pagination', type: 'fraction' },
                     navigation: {
                         nextEl: '#swiperRuang .swiper-button-next',
@@ -2698,7 +2704,7 @@
                     slidesPerView: 1.2,
                     centeredSlides: true,
                     spaceBetween: 24,
-                    autoplay: { delay: 3000, disableOnInteraction: true },
+                    autoplay: { delay: 5000, disableOnInteraction: true },
                     pagination: { el: '#swiperAsrama .swiper-pagination', type: 'fraction' },
                     navigation: {
                         nextEl: '#swiperAsrama .swiper-button-next',
@@ -2716,7 +2722,7 @@
                     slidesPerView: 1.2,
                     centeredSlides: true,
                     spaceBetween: 24,
-                    autoplay: { delay: 3000, disableOnInteraction: true },
+                    autoplay: { delay: 5000, disableOnInteraction: true },
                     pagination: { el: '#swiperLaboratorium .swiper-pagination', type: 'fraction' },
                     navigation: {
                         nextEl: '#swiperLaboratorium .swiper-button-next',
@@ -2731,7 +2737,7 @@
             };
 
             // modal open close
-                function openModal(facility) {
+            function openModal(facility) {
                 const modal = document.getElementById('modal' + facility.charAt(0).toUpperCase() + facility.slice(1));
                 if (modal) {
                     modal.classList.remove('hidden');
